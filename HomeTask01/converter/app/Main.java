d188df71b00caf5c139e9b5409ebc7ac579f132c
aartemrrtgmail.com <aartemrrt@gmail.com>
2024-10-05 02:16:10 +0400

    line 8 modified

--- HomeTask01/converter/app/Main.java
+++ HomeTask01/converter/app/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static final double POUNDS_TO_KGS=0.453592;
 
     public static void main(String[] args) {
+        System.out.println("App for measures converting.");
         double p=5.0;
         double kg=pounds_to_kilos(p);
         System.out.printf("%.2f pounds are equals to %.2f kilos", p, kg);
