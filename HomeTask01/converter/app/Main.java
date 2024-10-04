09a98e6c7c397429a16c083c45bada20513432c1
aartemrrtgmail.com <aartemrrt@gmail.com>
2024-10-04 23:53:53 +0400

    first commit

---
+++ HomeTask01/converter/app/Main.java
@@ -0,0 +1,15 @@
+package converter.app;
+
+public class Main {
+
+    public static final double POUNDS_TO_KGS=0.453592;
+
+    public static void main(String[] args) {
+        double p=5.0;
+        double kg=pounds_to_kilos(p);
+        System.out.printf("%.2f pounds are equals to %.2f kilos", p, kg);
+    }
+    public static double pounds_to_kilos(double pounds){
+        return pounds*POUNDS_TO_KGS;
+    }
+}
