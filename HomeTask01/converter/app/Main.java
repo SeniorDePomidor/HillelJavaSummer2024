52ed324466ca9298752778790390c9279738ff29
aartemrrtgmail.com <aartemrrt@gmail.com>
2024-10-05 00:07:40 +0400

    cherry picked
    (cherry picked from commit cfd92e01ff5d69f71052802394624ca46174b56b)

--- HomeTask01/converter/app/Main.java
+++ HomeTask01/converter/app/Main.java
@@ -5,15 +5,15 @@ public class Main {
     public static final double POUNDS_TO_KGS=0.453592;
 
     public static void main(String[] args) {
+        System.out.println("App for measures converting.");
         System.out.println("Version 1.0");
-        double p=5;
+        double p=5.0;
         double kg=pounds_to_kilos(p);
         System.out.printf("%.2f pounds are equals to %.2f kilos%n", p, kg);
         kg=12;
         p=kilos_to_pounds(kg);
         System.out.printf("%.2f kilos are equals to %.2f pounds%n", kg, p);
     }
-
     public static double pounds_to_kilos(double pounds){
         return pounds*POUNDS_TO_KGS;
     }
