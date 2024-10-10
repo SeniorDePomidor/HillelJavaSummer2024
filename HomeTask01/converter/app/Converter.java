125d27328f29a15714aa08a84d14f51bd2a7f83b
aartemrrtgmail.com <aartemrrt@gmail.com>
2024-10-10 07:21:57 +0400

    conversion km to miles added

--- HomeTask01/converter/app/Converter.java
+++ HomeTask01/converter/app/Converter.java
@@ -8,5 +8,7 @@ public class Converter {
     public static void main(String[] args) {
         System.out.printf("%.2f miles are equal to %.2f kilometres\n", Double.parseDouble(args[0]),
                 Double.parseDouble(args[0]) * KM_PER_MILE);
+        System.out.printf("%.2f kilometres are equal to %.2f miles\n", Double.parseDouble(args[1]),
+                Double.parseDouble(args[1]) / KM_PER_MILE);
     }
 }
