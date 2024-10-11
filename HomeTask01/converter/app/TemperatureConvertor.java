1d442530f67752f10efac6692d96424af0632e43
aartemrrtgmail.com <aartemrrt@gmail.com>
2024-10-12 02:24:31 +0400

    method for conversion F to C added

--- HomeTask01/converter/app/TemperatureConvertor.java
+++ HomeTask01/converter/app/TemperatureConvertor.java
@@ -3,5 +3,11 @@ package converter.app;
 public class TemperatureConvertor {
     public static void main(String[] args) {
         System.out.println("Application for conversion of temperature");
+        double fahrenheitTemp = 27.0;
+        System.out.printf("%.2f Fahrenheits are equals to %.2f Celsius\n", fahrenheitTemp,
+                fromFahrenheitToCelsius(fahrenheitTemp));
+    }
+    public static double fromFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit-32) / 1.8;
     }
 }
